013c3d64441b5f9667bc695ce34dc2797aa261dc
ARMkiyas <armkiyas99@hotmail.com>
2022-02-23 02:50:41 +0530

    master

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://arm-thread-backend.herokuapp.com",
         changeOrigin: true,
         secure: false,
         ws: true,
