d0fbb45fdbc9ea07cb34d5651e54725da253785d
ARMkiyas <armkiyas99@hotmail.com>
2022-03-04 22:21:36 +0530

    added new feture to pdf

--- src/components/pdf_gen.jsx
+++ src/components/pdf_gen.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import pdf from "@react-pdf/renderer";
 import { connect } from "react-redux";
 
-const { Page, Text, Image, Document, View, StyleSheet,Font } = pdf;
-
+const { Page, Text, Image, Document, View, Svg, Line, StyleSheet, Font } = pdf;
+let date=new Date();
 
 const PDFFile = (props) => {
-  
   Font.register({
     family: "Roboto",
-    src:
-      "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf"
+    src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf",
   });
-  
+
   console.log(props.getData);
   props?.getData?.data?.map((tweets) => {
     let tw = tweets.text;
@@ -32,13 +30,14 @@ const PDFFile = (props) => {
     text: {
       margin: 12,
       fontSize: 14,
-      textAlign: 'justify',
-      fontFamily: 'Roboto'
-    },text_img:{
+      textAlign: "justify",
+      fontFamily: "Roboto",
+    },
+    text_img: {
       margin: 0,
       fontSize: 14,
-      textAlign: 'justify',
-      fontFamily: 'Roboto'
+      textAlign: "justify",
+      fontFamily: "Roboto",
     },
     image: {
       marginVertical: 15,
@@ -61,13 +60,78 @@ const PDFFile = (props) => {
       textAlign: "center",
       color: "grey",
     },
+    cre_date: {
+      display: "flex",
+      alignSelf: "flex-end",
+      fontSize: 12,
+      margin: 4,
+    },
+    line: {
+      x1: "1000", //starting coords are x1 and y1
+      y1: "0",
+      x2: "0", //ending coords:
+      y2: "0",
+      strokeWidth: 2,
+      stroke: "rgb(219, 219, 219)",
+
+      //stroke color
+    },
+    author_view: {
+      margin: 10,
+      alignSelf: "center",
+    },
+    author: {
+      fontSize: 14,
+      marginTop: 4,
+    },
+    head: {
+      display: "flex",
+      flexDirection: "row",
+    },
+    pro_image: {
+      width: 80,
+    },
   });
   console.log(props.getData);
-
+  console.log(props?.getData?.data?.length);
+  const arr_len=props?.getData?.data?.length;
+  if(arr_len%2==0 && arr_len>1){
+    const col=arr_len/2;
+  }else if(arr_len>1){
+    const col={
+      "1st":((arr_len-1)/2) + 1,
+      "2nd":(arr_len-1)/2
+    }
+  }else{
+    const col=1
+  }
   return (
     <Document>
       <Page style={styles.body}>
+        <View fixed>
+          <View style={styles.head}>
+            <Image
+              style={styles.pro_image}
+              src={props.getData.author_info.profile_image_url.replace("_normal","")}
+            />
+            <View style={styles.author_view}>
+              <Text style={styles.author}>Thread Author: {props.getData.author_info.name}</Text>
+
+              <Text style={styles.author}>Publication Data: {props.getData.created_at.split("T")[0]}</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.cre_date}>Created at {
+             date.toLocaleString()
+            }</Text>
+          </View>
+
+          <Svg width={"100%"} height={"10"}>
+            <Line style={styles.line} />
+          </Svg>
+        </View>
         <View>
+          
           {props?.getData?.data?.map((tweets) => {
             let media_data = {};
             media_data = tweets?.attachments
@@ -78,21 +142,20 @@ const PDFFile = (props) => {
                 })
               : {};
             return (
+              
               <div key={tweets.id}>
                 <Text style={styles.text}>
                   {" "}
-                  {tweets.text.replace(/\n/g, "")}     </Text>
+                  {tweets.text.replace(/\n/g, "")}{" "}
+                </Text>
                 <Text style={styles.text}>
-                {Object.keys(media_data).length >= 0 &&
+                  {Object.keys(media_data).length >= 0 &&
                   media_data.type !== "photo" ? (
                     ""
                   ) : (
-     
                     <Image style={styles.image} src={media_data?.url} />
                   )}
                 </Text>
-                  
-           
               </div>
             );
           })}
@@ -130,12 +193,13 @@ const PDFFile = (props) => {
             }
           />
         </div> */}
-                <Text
-            style={styles.pageNumber}
-            render={({ pageNumber, totalPages }) =>
-              `${pageNumber} / ${totalPages}`
-            }
-            fixed />
+        <Text
+          style={styles.pageNumber}
+          render={({ pageNumber, totalPages }) =>
+            `${pageNumber} / ${totalPages}`
+          }
+          fixed
+        />
       </Page>
     </Document>
   );
