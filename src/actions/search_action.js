0c1e02f3b36de9d8c24738ec1d6373deec715e5e
ARMkiyas <armkiyas99@hotmail.com>
2022-07-28 00:43:37 +0530

    Update search_action.js

--- src/actions/search_action.js
+++ src/actions/search_action.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export const get_data=(id , loading)=>{
     loading(true)
   return (dispatch)=>{
-     axios.get(`https://bobbin-backend.herokuapp.com/api/getdata/${id}`).then(({data})=>{
+     axios.get(`https://arm-thread-backend.herokuapp.com/api/getdata/${id}`).then(({data})=>{
             loading(false)
         
             return dispatch({type:"FETCH_THREAD", payload:data})
